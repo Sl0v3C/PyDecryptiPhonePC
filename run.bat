bab48916beb4789479a6c3018ae5ff17ed63d2b4
Sl0v3C <pyy101727@gmail.com>
2017-08-24 14:57:18 +0800

    [PyDecryptiPhonePC] Add run.bat for windows version usage
    Add run.bat for windows version usage

---
+++ run.bat
@@ -0,0 +1,5 @@
+::Please replace the "RestrictionsPasswordKey" and "RestrictionsPasswordSalt"
+::Like: decryption.exe "axo6VEI3Tn2ekTeLxA8KBBmH7CA=" "GuCyTQ=="
+
+decryption.exe "RestrictionsPasswordKey" "RestrictionsPasswordSalt"
+pause
